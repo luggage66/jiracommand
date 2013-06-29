f2d2b11ed5b50825cb9226e18b34dd0944b9cf3a
Donald Mull Jr <donald.mull@gmail.com>
2013-06-29 19:40:50 -0400

    more setup

--- jiracommand/CommandArgs.cs
+++ jiracommand/CommandArgs.cs
@@ -12,25 +12,38 @@ namespace jiracommand
     [ArgExample("jiracommand login -user dmull", "login with a password from a prompt")]
     public class CommandArgs
     {
-        [ArgActionMethod, ArgDescription("See this screen")]
-
-        public void Help(HelpArgs args)
-        {
-            ArgUsage.GetStyledUsage<CommandArgs>(null, new ArgUsageOptions() { ShowType = false }).Write();
-        }
+        static string serverFile = ".jiraserver";
 
         [ArgActionMethod, ArgDescription("gets an auth token from the server")]
-        
         public void Login(LoginArgs args)
         {
             Console.WriteLine(args.User);
             Console.WriteLine(args.Password.ConvertToNonsecureString());
         }
 
+        [ArgActionMethod, ArgDescription("Create a Jira issue")]
+        public void AddIssue(AddIssueArgs args)
+        {
+            Console.WriteLine("A   WPR-1234");
+        }
+
+        [ArgActionMethod, ArgDescription("See this screen")]
+        public void Help(HelpArgs args)
+        {
+            ArgUsage.GetStyledUsage<CommandArgs>(null, new ArgUsageOptions() { ShowType = false }).Write();
+        }
+
         [ArgActionMethod, ArgDescription("change setup")]
         public void Config(ConfigArgs args)
         {
+            switch (args.Action)
+            {
+                case ConfigAction.setserver:
+                    System.IO.File.WriteAllText(serverFile, args.Value);
+                    break;
+            }
         }
+
 
     }
 
@@ -38,14 +51,20 @@ namespace jiracommand
     {
     }
 
+    public class AddIssueArgs
+    {
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string DescriptionFile { get; set; }
+    }
     
     public class ConfigArgs
     {
-        [ArgPosition(0)]
+        [ArgPosition(1)]
         public ConfigAction Action { get; set; }
 
 
-        [ArgPosition(1)]
+        [ArgPosition(2)]
         public string Value { set; get; }
     }
 
@@ -58,6 +77,8 @@ namespace jiracommand
     public class LoginArgs
     {
         public string User { get; set; }
+
+        [ArgDescription("Password. You can skip and provide it interactively, too.")]
         public SecureStringArgument Password { get; set; }
     }
 }
