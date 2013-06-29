2e08ae6139a10b1dc70ea480e0c7c1388f2c2d83
Donald Mull Jr <donald.mull@gmail.com>
2013-06-29 11:16:14 -0400

    Initial commit

---
+++ jiracommand/CommandArgs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PowerArgs;
+
+namespace jiracommand
+{
+    public class CommandArgs
+    {
+        [ArgRequired]
+        [ArgPosition(0)]
+        [ArgDescription("")]
+        public string Action { get; set; }
+
+        public static void Help(HelpArgs args)
+        {
+            ArgUsage.GetStyledUsage<CommandArgs>().Write();
+        }
+
+        public static void Login(LoginArgs args)
+        {
+            Console.WriteLine(args.User);
+            Console.WriteLine(args.Password.ConvertToNonsecureString());
+        }
+
+        public LoginArgs LoginArgs { get; set; }
+        public HelpArgs HelpArgs { get; set; }
+    }
+
+    public class HelpArgs
+    {
+    }
+
+    public class LoginArgs
+    {
+        public string User { get; set; }
+        public SecureStringArgument Password { get; set; }
+    }
+}
---
+++ jiracommand/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PowerArgs;
+
+namespace jiracommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Args.InvokeAction<CommandArgs>(args);
+            }
+            catch (PowerArgs.ArgException argEx)
+            {
+                Console.WriteLine(argEx.Message);
+                ArgUsage.GetStyledUsage<CommandArgs>().Write();
+            }
+
+        }
+    }
+}
